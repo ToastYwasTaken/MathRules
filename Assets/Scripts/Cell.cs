f6be39f216c30c02f6a0ed5035cde864e0c8931b
ToastYwasTaken <franz@emoerike.de>
2021-11-14 21:41:41 +0100

    added functionality
    still have to fix the mats

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,81 +1,150 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System;
-
+using UnityEngine;
+/******************************************************************************
+ * Project: MathRules
+ * File: Cell.cs
+ * Version: 1.01
+ * Autor:  Franz Mörike (FM);
+ * 
+ * 
+ * These coded instructions, statements, and computer programs contain
+ * proprietary information of the author and are protected by Federal
+ * copyright law. They may not be disclosed to third parties or copied
+ * or duplicated in any form, in whole or in part, without the prior
+ * written consent of the author.
+ * 
+ * NOTE:
+ * 
+ * The basic coding structure and algorithms are from Unterricht_GenerischeWelten!
+ * That code-base was used, improved and adjusted to my usage.
+ * 
+ * ChangeLog
+ * ----------------------------
+ *  11.11.2021  created
+ *  13.11.2021  added comments
+ *  14.11.2021  added code
+ *  
+ *****************************************************************************/
 public class Cell : MonoBehaviour
 {
     private CellularAutomata cellularAutomata;
-    [SerializeField]
     private Renderer rend;
-    [Header("Flamable or inflamable")]
+
+    //Flamable or inflamable
     private Material materialInflamable;
     private Material materialFlamable;
-    [Header("Burning mats")]
+
+    //Burning mats
     private Material materialBurningSlightly;
     private Material materialBurningNormal;
     private Material materialBurningExtremly;
 
     private Vector2Int idxInArray;
+    private int seedIntForCells;
 
-    //Callback for burning
-    private Action<Vector2Int, bool> callBack;
-    
-    private bool burning;
+    //Callback for [first bool] flamable and [second bool] burning
+    private Action<Vector2Int, bool, bool> callback;
     private bool flamable;
+    private bool burning;
+    private EInternalStates state;
 
     private void Awake()
     {
         rend = this.gameObject.GetComponent<Renderer>();
     }
 
-    public void AssignCell(CellularAutomata _cellularAutomata, Material _materialInflamable, Material _materialFlamable, Material _materialBurningSlightly, Material _materialBurningNormal, Material _materialBurningExtremly, int idxX, int idxY, Action<Vector2Int, bool> _callback, bool _flamable)
+    /// <summary>
+    /// Used to assign the cells from CellularAutomata
+    /// </summary>
+    /// <param name="_cellularAutomata">reference to the Cellular automata</param>
+    /// <param name="_materialInflamable">material</param>
+    /// <param name="_materialFlamable">material</param>
+    /// <param name="_materialBurningSlightly">material</param>
+    /// <param name="_materialBurningNormal">material</param>
+    /// <param name="_materialBurningExtremly">material</param>
+    /// <param name="_seedIntForCells">seed used to spread fire</param>
+    /// <param name="idxX">X coordinate of the cell in the 2D array of cells </param>
+    /// <param name="idxY">Y coordinate of the cell in the 2D array of cells </param>
+    /// <param name="_callback">Callback for igniting fire</param>
+    /// <param name="_flamable">default: false</param>
+    /// <param name="_burning">default: false</param>
+    public void AssignCell(CellularAutomata _cellularAutomata, Material _materialInflamable, Material _materialFlamable, Material _materialBurningSlightly, Material _materialBurningNormal, Material _materialBurningExtremly, int _seedIntForCells, int idxX, int idxY, Action<Vector2Int, bool, bool> _callback, bool _flamable = false, bool _burning = false)
     {
         cellularAutomata = _cellularAutomata;
+        idxInArray = new Vector2Int(idxX, idxY);
+
         materialInflamable = _materialInflamable;
         materialFlamable = _materialFlamable;
         materialBurningSlightly = _materialBurningSlightly;
         materialBurningNormal = _materialBurningNormal;
         materialBurningExtremly = _materialBurningExtremly;
-        idxInArray = new Vector2Int(idxX, idxY);
-        callBack = _callback;
+
+        callback = _callback;
         flamable = _flamable;
-        callBack = _callback;
-        SetFlamableMaterial(flamable);
+        burning = _burning;
 
-    }
-
-    //TODO: implement fire spreading
-    private void SetFlamableMaterial(bool _flamable)
-    {
-        if (_flamable)
-        {
-            if (true)
-            {
-
-            }
-            else if (true)
-            {
-
-            }
-            else if (true)
-            {
-
-            }
-            else return;
-        }
-        else SetBurningMaterial(burning ? anyBurningMat : anyNonBurningMat);
-    }
-
-    private void SetBurningMaterial()
-    {
-
-    }
-
-    public void UpdateCellStatus(bool _flamable)
-    {
-        flamable = _flamable;
+        seedIntForCells = _seedIntForCells;
+        // Set flamable Material if cell is flamable -> else set inflamable material
+        Debug.Log($"flamable: {flamable} burning: {burning}");
         SetFlamableMaterial(flamable ? materialFlamable : materialInflamable);
+    }
+
+    /// <summary>
+    /// Set foundary material of cell
+    /// </summary>
+    /// <param name="_material"></param>
+    private void SetFlamableMaterial(Material _material)
+    {
+        rend.material = _material;
+    }
+
+    //TODO: implement fire spreading here
+    /// <summary>
+    /// Change the material accordingly when fire is set
+    /// </summary>
+    /// <param name="_materialBurningSlightly">material</param>
+    /// <param name="_materialBurningNormal">material</param>
+    /// <param name="_materialBurningExtremely">material</param>
+    private void SetRandomBurningMaterial(Material _materialBurningSlightly, Material _materialBurningNormal, Material _materialBurningExtremely)
+    {
+        System.Random rdm = new System.Random(seedIntForCells);
+        //1 rdm int for each Material
+        int rdmIntBurning = rdm.Next(0, 3);
+        //burning slightly
+        if (rdmIntBurning == (int)EInternalStates.E_INFLAMABLE)
+        {
+            rend.material = _materialBurningSlightly;
+        }
+        //burning normal
+        else if (rdmIntBurning == (int)EInternalStates.E_FLAMABLE)
+        {
+            rend.material = _materialBurningNormal;
+        }
+        //burning extremely;
+        else if (rdmIntBurning == (int)EInternalStates.E_BURNING)
+        {
+            rend.material = _materialBurningExtremely;
+        }
+        else return;
+    }
+
+    /// <summary>
+    /// Updates the cells in case they switched states
+    /// </summary>
+    /// <param name="_flamable">state passed by Cellular automata</param>
+    /// <param name="_burning">state passed by Cellular automata</param>
+    public void UpdateCellStatus(bool _flamable, bool _burning)
+    {
+        flamable = _flamable;
+        burning = _burning;
+        if (burning)
+        {
+            SetRandomBurningMaterial(materialBurningSlightly, materialBurningNormal, materialBurningExtremly);
+        }
+        else
+        {
+            SetFlamableMaterial(flamable ? materialFlamable : materialInflamable);
+        }
     }
 
     private void OnMouseOver()
@@ -94,8 +163,28 @@ public class Cell : MonoBehaviour
         }
         if (switched)
         {
-            callBack(idxInArray, burning);
+            callback(idxInArray, flamable, burning);
         }
     }
+
+    public EInternalStates GetCurrentState()
+    {
+        if (!flamable)
+        {
+            state = EInternalStates.E_INFLAMABLE;
+        }
+        else if (flamable && !burning)
+        {
+            state = EInternalStates.E_FLAMABLE;
+        }
+        else if (flamable && burning)
+        {
+            state = EInternalStates.E_BURNING;
+        }
+        else state = EInternalStates.E_INVALID;
+
+        return state;
+    }
+
 
 }
--- Assets/Scripts/CellularAutomata.cs
+++ Assets/Scripts/CellularAutomata.cs
@@ -1,20 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+/******************************************************************************
+ * Project: MathRules
+ * File: CellularAutomata.cs
+ * Version: 1.01
+ * Autor:  Franz Mörike (FM);
+ * 
+ * 
+ * These coded instructions, statements, and computer programs contain
+ * proprietary information of the author and are protected by Federal
+ * copyright law. They may not be disclosed to third parties or copied
+ * or duplicated in any form, in whole or in part, without the prior
+ * written consent of the author.
+ * 
+ * NOTE:
+ * 
+ * The basic coding structure and algorithms are from Unterricht_GenerischeWelten!
+ * That code-base was used, improved and adjusted to my usage.
+ * 
+ * The States are internally defined as [0] inflamable [1] flamable [2] burning
+ * 
+ * ChangeLog
+ * ----------------------------
+ *  11.11.2021  created
+ *  13.11.2021  added comments
+ *  14.11.2021  added public enum, additional code, added Text-functionality
+ *  
+ *****************************************************************************/
+public enum EInternalStates
+{
+    E_INVALID = -1,
+    E_INFLAMABLE = 0,
+    E_FLAMABLE = 1,
+    E_BURNING = 2
+}
 public class CellularAutomata : MonoBehaviour
-{   
+{
     [Header("Default settings")]
     [SerializeField]
     private int fps;
     [SerializeField, Range(0.1f, 2f)]
     private float simulationSpeed;
+    [SerializeField]
+    private bool displayStates;
+    [SerializeField]
+    private int fontSize;
+    [SerializeField]
+    private Color textColor;
+    [Tooltip("zoom in / zoom out | Range: 0f - 50f")]
+    [SerializeField, Range(0f, 50f)]
+    private float cameraZoom;
 
     [Header("Cell settings")]
     [SerializeField]
     private GameObject cellPrefab;
     [SerializeField]
     private float cellSize;
+    [SerializeField]
+    private GameObject textPrefab;
 
     //TODO possibly add more mats
     [Header("Cell materials")]
@@ -29,7 +74,6 @@ public class CellularAutomata : MonoBehaviour
     [SerializeField]
     private Material materialBurningExtremely;  //deep red
 
-
     [Header("Map settings")]
     [SerializeField]
     private int width;
@@ -42,15 +86,24 @@ public class CellularAutomata : MonoBehaviour
     [Tooltip("For no seed enter 'noSeed' or nothing")]
     [SerializeField]
     private string seedString;
+    [Tooltip("For no seed enter 'noSeed' or nothing")]
+    [SerializeField]
+    private string seedStringForCells;
 
     private int[,] map;
     private Cell[,] allCells;
     private int seedInt;
     private const int defaultMapValue = 0;
     private float timer;
+    private Camera cameraRef;
+    private const float defaultCamDistance = 25f;
+    private float currentCamDistance;
 
     private void Awake()
     {
+        cameraRef = FindObjectOfType<Camera>();
+        cameraRef.transform.position = new Vector3(0f, defaultCamDistance, 0f);
+        currentCamDistance = defaultCamDistance;
         map = GenerateMap(width, height, randomlyGenerated, fillPercent);
     }
     // Start is called before the first frame update
@@ -62,6 +115,11 @@ public class CellularAutomata : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (cameraZoom != currentCamDistance)
+        {
+            CameraZoom();
+            currentCamDistance = cameraZoom;
+        }
         map = OneStepInGame(map);
     }
 
@@ -91,6 +149,14 @@ public class CellularAutomata : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// Generate the map before spawning it.
+    /// </summary>
+    /// <param name="_width">desired width of the map</param>
+    /// <param name="_height">desired height of the map</param>
+    /// <param name="_randomlyGenerated">should the map be randomly generated?</param>
+    /// <param name="_fillPercent">how much percent of the map should be 'active'</param>
+    /// <returns></returns>
     private int[,] GenerateMap(int _width, int _height, bool _randomlyGenerated, float _fillPercent)
     {
         seedInt = GenerateSeedStringToInt(seedString);
@@ -101,7 +167,7 @@ public class CellularAutomata : MonoBehaviour
         {
             for (int j = 0; j < _width; j++)
             {
-                //randomly set cells to 1 or 0 (active/inactive)
+                //randomly set cells to 1 or 0 (active/inactive in this case flamable / inflamable)
                 if (_randomlyGenerated)
                 {
                     tempMap[j, i] = (float)rdm.NextDouble() * 100f <= fillPercent ? 1 : 0;
@@ -113,23 +179,74 @@ public class CellularAutomata : MonoBehaviour
         return tempMap;
     }
 
+    /// <summary>
+    /// Spawning the game map consisting of cubes
+    /// </summary>
+    /// <param name="map">the map generated previously in GenerateMap()</param>
+    /// <param name="_mapHeight">the map's desired height</param>
+    /// <param name="_mapWidth">the map's desired width</param>
+    /// <param name="_size">the tile size</param>
+    /// <returns></returns>
     private Cell[,] SpawnMap(int [,] map, int _mapHeight, int _mapWidth, float _size)
     {
         Cell[,] spawnedCells = new Cell[_mapWidth, _mapHeight];
-        for (int i = 0; i < _mapHeight; i++)
+        //Pass a seed to cell
+        int seedIntForCells = GenerateSeedStringToInt(seedStringForCells);
+        //make map centered at (0,0,0)
+        Vector3 spawnOffset = new Vector3(_mapWidth, 0, _mapHeight) * -0.5f;
+        Vector3 textOffset = new Vector3(0, 2, 0);
+        //map spawning
+        if (displayStates)
         {
-            for (int j = 0; j < _mapWidth; j++)
+            for (int i = 0; i < _mapHeight; i++)
             {
-                GameObject cellGO = Instantiate(cellPrefab);
-                cellGO.name = $"Cell[{j}|{i}]";
-                cellGO.transform.localPosition = new Vector3(j, 0, i) * _size;
-                spawnedCells[j, i] = cellGO.GetComponent<Cell>();
-                spawnedCells[j, i].AssignCell(this, materialInflamable, materialFlamable, materialBurningSlightly, materialBurningNormal, materialBurningExtremely, j, i, OnCellChanged, map[j, i] == 1);
+                for (int j = 0; j < _mapWidth; j++)
+                {
+                    //Spawn cell
+                    GameObject cellGO = Instantiate(cellPrefab);
+                    cellGO.name = $"Cell[{j}|{i}]";
+                    cellGO.transform.localPosition = spawnOffset + new Vector3(j, 0, i) * _size;
+                    //text preparations
+                    GameObject textGO = Instantiate(textPrefab);
+                    TextMeshPro textMesh = textPrefab.transform.GetComponent<TextMeshPro>();
+                    textGO.name = cellGO.name + " text";
+                    textMesh.fontSize = (int)fontSize;
+                    textMesh.color = textColor;
+                    textGO.transform.localPosition = textOffset + cellGO.transform.localPosition;
+                    textGO.transform.Rotate(90f, 0, 0, Space.Self);
+                    //Assign cells
+                    spawnedCells[j, i] = cellGO.GetComponent<Cell>();
+                    spawnedCells[j, i].AssignCell(this, materialInflamable, materialFlamable, materialBurningSlightly, materialBurningNormal, materialBurningExtremely, seedIntForCells, j, i, OnCellChanged, map[j, i] == 1);
+                    //Assign / spawn text
+                    textMesh.text = ((int)(spawnedCells[j, i].GetCurrentState())).ToString();
+                }
+            }
+        }else
+        {
+            for (int i = 0; i < _mapHeight; i++)
+            {
+                for (int j = 0; j < _mapWidth; j++)
+                {
+                    //Spawn cell
+                    GameObject cellGO = Instantiate(cellPrefab);
+                    cellGO.name = $"Cell[{j}|{i}]";
+                    cellGO.transform.localPosition = spawnOffset + new Vector3(j, 0, i) * _size;
+
+                    //Assign cells
+                    spawnedCells[j, i] = cellGO.GetComponent<Cell>();
+                    spawnedCells[j, i].AssignCell(this, materialInflamable, materialFlamable, materialBurningSlightly, materialBurningNormal, materialBurningExtremely, seedIntForCells, j, i, OnCellChanged, map[j, i] == 1);
+                }
             }
         }
+
         return spawnedCells;
     }
 
+    /// <summary>
+    /// Applies the rules for the algorithm
+    /// </summary>
+    /// <param name="_map">current map</param>
+    /// <returns>updated map</returns>
     private int[,] OneStepInGame(int[,] _map)
     {
         int newWidth = _map.GetLength(0);
@@ -141,36 +258,36 @@ public class CellularAutomata : MonoBehaviour
             for (int j = 0; j < newWidth; j++)
             {
                 //get all active neighbours | max 8 active neighbours
-                int activeNeighbours = GetActiveNeighbours(newMap, newHeight, newWidth, j, i);
+                int activeNeighbours = GetActiveNeighbours(_map, j, i);
 
                 //Rules here
                 //Cell active
-                if (_map[j, i] == 1)
+                if (_map[j, i] == (int)EInternalStates.E_FLAMABLE)
                 {
-                    //Rule 1: if less than 3 neighbours -> inactive
+                    //Rule 1: if less than 3 neighbours -> inflamable
                     if (activeNeighbours <= 2)
                     {
-                        newMap[j, i] = 0;
+                        newMap[j, i] = (int)EInternalStates.E_INFLAMABLE;
                     }
-                    //Rule 2: if more than 8 neighbours -> stay active
+                    //Rule 2: if more than 8 neighbours -> stay flamable
                     else if (activeNeighbours >= 9)
                     {
-                        newMap[j, i] = 1;
+                        newMap[j, i] = (int)EInternalStates.E_BURNING;
                     }
                     //Rule 3: if >2 and <9 -> randomly set status
                     else
                     {
-                        newMap[j,i] = RandomlyReturn1or0();
+                        newMap[j,i] = RandomStatus();
                     }
                 //Cell inactive
                 }else if (_map[j,i] == 0)
                 {
-                    //Rule 4: if less than 7 neighbours -> stay inactive
+                    //Rule 4: if less than 7 neighbours -> stay inflamable
                     if (activeNeighbours <=6)
                     {
                         newMap[j, i] = 0;
                     }
-                    //Rule 5: if more than 8 neighbours -> active
+                    //Rule 5: if more than 8 neighbours -> flamable
                     else if (activeNeighbours >= 9)
                     {
                         newMap[j, i] = 1;
@@ -178,12 +295,8 @@ public class CellularAutomata : MonoBehaviour
                     //Rule 6: if >6 and <9 -> randomly set status
                     else
                     {
-                        newMap[j, i] = RandomlyReturn1or0();
+                        newMap[j, i] = RandomStatus();
                     }
-                }else
-                //shouldn't be reached anyways
-                {
-
                 }
 
             }
@@ -192,8 +305,19 @@ public class CellularAutomata : MonoBehaviour
         return newMap;
     }
 
-    private int GetActiveNeighbours(int[,] _map, int _newMapHeight, int _newMapWidth, int _posOnMapX, int _posOnMapY)
+    /// <summary>
+    /// Gets the neighbourcount of a Cell at posX, poxY on _map
+    /// </summary>
+    /// <param name="_map">the map which neighbourcount is wished to be determinated</param>
+    /// <param name="_newMapHeight"></param>
+    /// <param name="_newMapWidth"></param>
+    /// <param name="_posOnMapX"></param>
+    /// <param name="_posOnMapY"></param>
+    /// <returns>count of active neighbours</returns>
+    private int GetActiveNeighbours(int[,] _map, int _posOnMapX, int _posOnMapY)
     {
+        int mapWidth = _map.GetLength(0);
+        int mapHeight = _map.GetLength(1);
         int neighbourCount = 0;
 
         for (int i = _posOnMapY - 1; i <= _posOnMapY + 1; i++)
@@ -206,9 +330,10 @@ public class CellularAutomata : MonoBehaviour
                     continue;
                 }
                 // check if on map
-                if(i >= 0 && i < _newMapHeight && j >= 0 && j < _newMapWidth)
+                if(i >= 0 && i < mapHeight && j >= 0 && j < mapWidth)
                 {
-                    //alive
+                    //if flamable
+                    //TODO: do burning cells count?
                     if (_map[j,i] == 1)
                     {
                         neighbourCount++;
@@ -219,29 +344,61 @@ public class CellularAutomata : MonoBehaviour
         return neighbourCount;
     }
 
+    /// <summary>
+    /// Updates the map after each step of the algorithm or when a cell's state changed
+    /// </summary>
+    /// <param name="_map">the map to be updated</param>
+    /// <param name="_mapHeight">the map's height</param>
+    /// <param name="_mapWidth">the map's width</param>
     private void UpdateMap(int[,] _map, int _mapHeight, int _mapWidth)
     {
         for (int i = 0; i < _mapHeight; i++)
         {
             for (int j = 0; j < _mapWidth; j++)
             {
-                //if 1 -> cell active
-                allCells[j, i].UpdateCellStatus(_map[j, i] == 1);
+                //Updates the state in the cells | if 1 -> cell active | if 2 -> burning 
+                allCells[j, i].UpdateCellStatus(_map[j, i] == (int)EInternalStates.E_FLAMABLE, _map[j, i] == (int)EInternalStates.E_BURNING);
             }
         }
     }
 
-    private void OnCellChanged(Vector2Int _position, bool _active)
+    /// <summary>
+    /// Callback from cell to see if a cell's state was changed
+    /// </summary>
+    /// <param name="_position">position of cell on the 2d array</param>
+    /// <param name="_flamable"></param>
+    /// <param name="_flamable"></param>
+    /// <param name="_burning"></param>
+    private void OnCellChanged(Vector2Int _position, bool _flamable, bool _burning)
     {
-        map[_position.x, _position.y] = _active ? 1 : 0;
+        if (_flamable)
+        {
+            map[_position.x, _position.y] = (int)EInternalStates.E_FLAMABLE;
+        }
+        else if (_burning)
+        {
+            map[_position.x, _position.y] = (int)EInternalStates.E_BURNING;
+        } else // if(_inflamable) not necessary tho
+        {
+            map[_position.x, _position.y] = (int)EInternalStates.E_INFLAMABLE;
+        }
         UpdateMap(map, map.GetLength(0), map.GetLength(1));
     }
 
-    private int RandomlyReturn1or0()
+    /// <summary>
+    /// Generates a number between 0 and 2 (incl)
+    /// </summary>
+    /// <returns>a random number in that range</returns>
+    private int RandomStatus()
     {
         seedInt = GenerateSeedStringToInt(seedString);
         System.Random rdm = new System.Random(seedInt);
-        int rdmInt = rdm.Next(0, 1);
+        int rdmInt = rdm.Next(0, 3);
         return rdmInt;
+    }
+
+    private void CameraZoom()
+    {
+        cameraRef.transform.position = new Vector3 (0f, cameraZoom, 0f);
     }
 }
